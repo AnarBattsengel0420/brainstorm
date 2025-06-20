17a84354859f0cf25ef030343e188dc35b1b5ce3
Anar <s21c076b@nmct.edu.mn>
2025-06-20 13:59:48 +0800

    ehluulsen

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="flex justify-center items-center text-3xl font-bold">OK</h1>
+    </div>
+  )
+}
+
+export default App
