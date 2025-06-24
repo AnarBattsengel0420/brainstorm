5b37c1f43d0a307de228bfb47954cd427022609e
Anar <s21c076b@nmct.edu.mn>
2025-06-24 20:08:31 +0800

    uurchlult zasav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className='text-blue-800'>Brainstorm</div>
+      <div className="text-blue-800">Brainstorm</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
